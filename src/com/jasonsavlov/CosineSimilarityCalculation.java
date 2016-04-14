de421538732b1fde4978dcf78ca60cf572a87ed5
Jason Savlov <jsavlov@oswego.edu>
2016-04-14 14:40:52 -0400

    Uploaded missing classes to GitHub

---
+++ src/com/jasonsavlov/CosineSimilarityCalculation.java
@@ -0,0 +1,101 @@
+package com.jasonsavlov;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by jason on 4/14/16.
+ */
+public class CosineSimilarityCalculation implements Callable<CosineSimilarityCalculation.CosineSimilarityResult>
+{
+
+    private final WebPage sourcePage, queryPage;
+
+
+    @Override
+    public CosineSimilarityResult call() throws Exception
+    {
+        List<Integer> listA = new ArrayList<>(), listB = new ArrayList<>();
+        List<String> wordsFromSource = sourcePage.getHashTable().getListOfWords();
+
+        wordsFromSource = sourcePage.getHashTable().getListOfWords();
+
+        for (String word : wordsFromSource)
+        {
+            int srcWordFreq = sourcePage.getHashTable().getFrequencyOfWord(word);
+            int qWordFreq = queryPage.getHashTable().getFrequencyOfWord(word);
+
+            listA.add(srcWordFreq);
+            listB.add(qWordFreq);
+        }
+
+        int dotProduct = calculateDotProduct(listA, listB);
+        double listANorm = calculateEuclidianNorm(listA);
+        double listBNorm = calculateEuclidianNorm(listB);
+
+        double result = (double) dotProduct / (listANorm * listBNorm);
+
+
+        return new CosineSimilarityResult(this.sourcePage.getPageURL(), result);
+    }
+
+
+
+    public CosineSimilarityCalculation(WebPage sourcePage, WebPage queryPage)
+    {
+        this.sourcePage = sourcePage;
+        this.queryPage = queryPage;
+    }
+
+    private int calculateDotProduct(List<Integer> listA, List<Integer> listB)
+    {
+        int resultToReturn = 0;
+
+        int number = 0;
+
+        if (listA.size() != listB.size()) return -1; // The lists arent the same size.. not good
+        else number = listA.size();
+
+        for (int i = 0; i < number; i++)
+        {
+            resultToReturn += listA.get(i) * listB.get(i);
+        }
+
+        return resultToReturn;
+    }
+
+    public double calculateEuclidianNorm(List<Integer> listOfFreqs)
+    {
+        double resultToReturn = 0;
+        int sqSum = 0;
+
+        for (Integer num : listOfFreqs)
+        {
+            sqSum += num * num;
+        }
+
+        resultToReturn = Math.sqrt((double) sqSum);
+        return resultToReturn;
+    }
+
+    public static class CosineSimilarityResult implements Comparable<CosineSimilarityResult> {
+        final String page;
+        final double similarity;
+
+        public CosineSimilarityResult(String page, double similarity)
+        {
+            this.page = page;
+            this.similarity = similarity;
+        }
+
+        @Override
+        public int compareTo(CosineSimilarityResult o)
+        {
+            if (o == this) return 0;
+
+            return o.similarity > this.similarity ? 1 : o.similarity < this.similarity ? -1 : 0;
+        }
+    }
+}
